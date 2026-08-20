b66eebed90f9b13aa615a6ccfce2776d6c73e40e
BoweloIPSPACE <medicalappmedapp@gmail.com>
2026-08-21 00:25:51 +0300

    Load workspace scale polish

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './refinement.css'
 import './clinical-saas.css'
+import './workspace-scale.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
