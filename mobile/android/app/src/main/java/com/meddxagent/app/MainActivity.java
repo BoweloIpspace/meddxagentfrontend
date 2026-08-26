aeaf1d19f5f04302ac4de602d74f54340ee2efad
BoweloIPSPACE <medicalappmedapp@gmail.com>
2026-08-26 16:29:06 +0300

    Handle Android system back navigation

--- mobile/android/app/src/main/java/com/meddxagent/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/meddxagent/app/MainActivity.java
@@ -2,4 +2,13 @@ package com.meddxagent.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
+            bridge.getWebView().goBack();
+            return;
+        }
+        super.onBackPressed();
+    }
+}
