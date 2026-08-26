2a26887a717bead0af709f0f36da003a124e285f
BoweloIPSPACE <medicalappmedapp@gmail.com>
2026-08-27 00:26:50 +0300

    Disable WebView debugging in Android release builds

--- mobile/android/app/src/main/java/com/meddxagent/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/meddxagent/app/MainActivity.java
@@ -1,8 +1,17 @@
 package com.meddxagent.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView.setWebContentsDebuggingEnabled(WebViewDebugPolicy.isEnabled(BuildConfig.DEBUG));
+    }
+
     @Override
     public void onBackPressed() {
         if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
